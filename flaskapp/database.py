4839cf9ceb45ba41a21df8fb1be60ae4a3289637
jmartinez996 <jmartinez2015@alu.uct.cl>
2021-11-12 12:21:12 -0300

    ..

--- flaskapp/database.py
+++ flaskapp/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ywtg.9819@localhost/robot'
-#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ywtg.9819@localhost:5434/robot'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ywtg.9819@localhost/robot'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ywtg.9819@localhost:5434/robot'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URI
